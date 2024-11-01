b3d762223b7ab1fc790d4dd4045d793953a50976
Le Tuan Phuc <letuanphuc@tofu.lan>
2024-11-01 15:31:20 +0700

    yolo with face detection

---
+++ compare.py
@@ -0,0 +1,66 @@
+from keras.models import Model, Sequential
+from keras.layers import Input, Convolution2D, ZeroPadding2D, MaxPooling2D, Flatten, Dense, Dropout, Activation
+from PIL import Image
+from keras.preprocessing.image import load_img, save_img, img_to_array
+from keras.applications.imagenet_utils import preprocess_input
+import numpy as np
+
+
+def loadVggFaceModel():
+	model = Sequential()
+	model.add(ZeroPadding2D((1,1),input_shape=(224,224, 3)))
+	model.add(Convolution2D(64, (3, 3), activation='relu'))
+	model.add(ZeroPadding2D((1,1)))
+	model.add(Convolution2D(64, (3, 3), activation='relu'))
+	model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+	model.add(ZeroPadding2D((1,1)))
+	model.add(Convolution2D(128, (3, 3), activation='relu'))
+	model.add(ZeroPadding2D((1,1)))
+	model.add(Convolution2D(128, (3, 3), activation='relu'))
+	model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+	model.add(ZeroPadding2D((1,1)))
+	model.add(Convolution2D(256, (3, 3), activation='relu'))
+	model.add(ZeroPadding2D((1,1)))
+	model.add(Convolution2D(256, (3, 3), activation='relu'))
+	model.add(ZeroPadding2D((1,1)))
+	model.add(Convolution2D(256, (3, 3), activation='relu'))
+	model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+	model.add(ZeroPadding2D((1,1)))
+	model.add(Convolution2D(512, (3, 3), activation='relu'))
+	model.add(ZeroPadding2D((1,1)))
+	model.add(Convolution2D(512, (3, 3), activation='relu'))
+	model.add(ZeroPadding2D((1,1)))
+	model.add(Convolution2D(512, (3, 3), activation='relu'))
+	model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+	model.add(ZeroPadding2D((1,1)))
+	model.add(Convolution2D(512, (3, 3), activation='relu'))
+	model.add(ZeroPadding2D((1,1)))
+	model.add(Convolution2D(512, (3, 3), activation='relu'))
+	model.add(ZeroPadding2D((1,1)))
+	model.add(Convolution2D(512, (3, 3), activation='relu'))
+	model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+	model.add(Convolution2D(4096, (7, 7), activation='relu'))
+	model.add(Dropout(0.5))
+	model.add(Convolution2D(4096, (1, 1), activation='relu'))
+	model.add(Dropout(0.5))
+	model.add(Convolution2D(2622, (1, 1)))
+	model.add(Flatten())
+	model.add(Activation('softmax'))
+	
+	
+	model.load_weights('vgg_face_weights.h5')
+	
+	vgg_face_descriptor = Model(inputs=model.layers[0].input, outputs=model.layers[-2].output)
+	
+	return vgg_face_descriptor
+
+def findCosineSimilarity(source_representation, test_representation) -> float :
+    a = np.matmul(np.transpose(source_representation), test_representation)
+    b = np.sum(np.multiply(source_representation, source_representation))
+    c = np.sum(np.multiply(test_representation, test_representation))
+    return 1 - (a / (np.sqrt(b) * np.sqrt(c)))
--- yolo.py
+++ yolo.py
@@ -1,6 +1,12 @@
+from os import listdir
+import numpy as np
 from ultralytics import YOLO
 import cv2
-import math 
+from keras.preprocessing import image
+from keras.applications.imagenet_utils import preprocess_input
+import math
+
+from compare import loadVggFaceModel, findCosineSimilarity
 # start webcam
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
@@ -8,7 +14,9 @@ cap.set(4, 480)
 
 # model
 # model = YOLO("yolo-Weights/yolov8n.pt")
-model = YOLO("yolo-Weights/yolo11m.pt")
+yolo_model = YOLO("yolo-Weights/yolov8n.pt")
+
+
 
 # object classes
 classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
@@ -23,13 +31,31 @@ classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "trai
               "teddy bear", "hair drier", "toothbrush"
               ]
 
+
 if not cap.isOpened():
     print("Error: Could not open camera.")
     exit()
 
+vgg_model = loadVggFaceModel()
+
+people_pictures = "./db"
+
+people = dict()
+def preprocess_image(image_path):
+    img = image.load_img(image_path, target_size=(224, 224))
+    img = image.img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+    
+    img = preprocess_input(img)
+    return img
+for file in listdir(people_pictures):
+	person, extension = file.split(".")
+	people[person] = vgg_model.predict(preprocess_image('./db/%s.jpg' % (person)))[0,:]
+
+
 while True:
     success, img = cap.read()
-    results = model(img, stream=True)
+    results = yolo_model(img, stream=True)
 
     # coordinates
     for r in results:
@@ -41,7 +67,7 @@ while True:
             # class name
             cls = int(box.cls[0])
             if classNames[cls] == "person":
-                 # bounding box
+                # bounding box
                 x1, y1, x2, y2 = box.xyxy[0]
                 x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
 
@@ -52,15 +78,33 @@ while True:
                 confidence = math.ceil((box.conf[0]*100))/100
                 print("Confidence --->",confidence)
                 print("Class name -->", classNames[cls])
+                detected_face = img[int(y1):int(y2), int(x1):int(x2)] 
+                detected_face = cv2.resize(detected_face, (224, 224))
+                img_pixels = image.img_to_array(detected_face)
+                img_pixels = np.expand_dims(img_pixels, axis = 0)
+                img_pixels /= 127.5
+                img_pixels -= 1
+                captured_representation = vgg_model.predict(img_pixels)[0,:]
+                found = 0
+                for person in people:
+                    person_name = person
+                    representation = people[person]
+                    
+                    similarity = findCosineSimilarity(representation, captured_representation)
+                    if(similarity < 0.20):
+                        print("Detect", person_name)
+                        org = [x1, y1]
+                        font = cv2.FONT_HERSHEY_SIMPLEX
+                        fontScale = 1
+                        color = (255, 0, 0)
+                        thickness = 2
+                        cv2.putText(img, person_name, org, font, fontScale, color, thickness)
+                        found = 1
+                        break
+                    
 
                 # object details
-                org = [x1, y1]
-                font = cv2.FONT_HERSHEY_SIMPLEX
-                fontScale = 1
-                color = (255, 0, 0)
-                thickness = 2
-
-                cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
+                
 
     cv2.imshow('Webcam', img)
     if cv2.waitKey(1) == ord('q'):
@@ -68,3 +112,9 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
+
+def findCosineSimilarity(source_representation, test_representation):
+    a = np.matmul(np.transpose(source_representation), test_representation)
+    b = np.sum(np.multiply(source_representation, source_representation))
+    c = np.sum(np.multiply(test_representation, test_representation))
+    return 1 - (a / (np.sqrt(b) * np.sqrt(c)))

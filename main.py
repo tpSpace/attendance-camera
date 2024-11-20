f25d32c821fb7ade7618ccc9d948f486418e5f42
tpSpace <ITCSIU21081@student.hcmiu.edu.vn>
2024-11-20 09:13:15 +0700

    Save

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from pipeline.pipeline import Pipeline
 if __name__ == "__main__":
     pipeline = Pipeline()
     pipeline.stream()
-    pipeline.from_image(path="./db/khoi.jpg")
+    # pipeline.from_image(path="./db/khoi.jpg")
